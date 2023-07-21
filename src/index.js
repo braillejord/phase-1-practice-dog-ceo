0a919804a2ab2300a91656c3c6c570e41a142fca
BreElle Wells <breellejordyn@gmail.com>
2023-07-21 10:25:14 -0600

    complete challenge one

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+// code that was previously here
 console.log('%c HI', 'color: firebrick')
+
+
+// challenge 1: fetch images, parse response, add elements to DOM
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl)
+    .then(r => r.json())
+    .then(data => renderImages(data.message))
+
+function renderImages(allImages) {
+    for (let image in allImages) {
+        const singleImageUrl = allImages[image]
+        const singleImage = document.createElement('img')
+        singleImage.src = singleImageUrl
+        const imageContainer = document.getElementById('dog-image-container')
+        imageContainer.appendChild(singleImage)
+    }
+}
